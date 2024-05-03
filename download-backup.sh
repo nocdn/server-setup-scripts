186bdd28e08c68dd1845ef4e1b0f6812cb138ec7
Kayetic <bartek.bak@protonmail.com>
2024-05-03 10:05:30 +0100

    added initial script

---
+++ download-backup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Bucket name
+bucket="modded-server-backups"
+
+echo "Fetching list of files from S3 bucket: $bucket..."
+
+# Get list of files sorted by last modified date (newest first)
+files=$(aws s3 ls s3://$bucket/ --recursive | sort -r | awk '{print $4}')
+
+if [ -z "$files" ]; then
+    echo "No files found in the bucket."
+    exit 1
+fi
+
+# Display files with an index number
+echo -e "Files in the bucket:\n"
+index=1
+for file in $files; do
+    echo "[$index]: $file"
+    index=$((index+1))
+done
+
+# Ask user to select a file to download
+echo -e "\nEnter number of file to download: "
+read file_number
+
+# Validate input
+re='^[0-9]+$'
+if ! [[ $file_number =~ $re ]] ; then
+   echo "Error: Not a number" >&2; exit 1
+fi
+
+# Convert input number to array index
+array_index=$((file_number-1))
+
+# Get filename from index
+file_names=($files)
+selected_file=${file_names[$array_index]}
+
+if [ -z "$selected_file" ]; then
+    echo "Invalid file number selected."
+    exit 1
+else
+    echo "You selected: $selected_file"
+fi
+
+# Download the selected file
+echo "Downloading $selected_file..."
+aws s3 cp s3://$bucket/$selected_file ./
+
+echo "$selected_file has been downloaded successfully."
---
+++ general.sh
@@ -0,0 +1,33 @@
+# check if the script is running as root:
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+# update sudo:
+apt-get update
+apt-get upgrade
+
+# install packages:
+apt-get install -y curl
+apt-get install -y unzip
+apt-get install -y zip
+apt install -y awscli
+
+# installing adoptium 17 jre:
+apt install -y wget apt-transport-https gpg
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+
+apt update
+apt install temurin-17-jre
+
+# configure aws cli:
+aws configure
+
+# exit root shell:
+exit
+
+# download the backup and restore scripts:
+
+curl -O 
