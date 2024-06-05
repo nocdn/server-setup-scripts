a6baab964f053339041f1bed74629a3e379ad4d0
Bartek <bartek.bak@protonmail.com>
2024-06-05 10:54:00 +0100

    added a personal script

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ curl -O "https://raw.githubusercontent.com/Kayetic/Server-Setup-Scripts/main/gen
 ```
 
 (This will download the script, make it executable, run it, and then delete it when it finishes.)
+
+For a personal setup script, run this:
+
+```bash
+curl -O "https://raw.githubusercontent.com/Kayetic/Server-Setup-Scripts/main/personal.sh" && chmod +x personal.sh && sudo bash personal.sh && rm personal.sh
+```
+
+(This will download the script, make it executable, run it, and then delete it when it finishes.)
---
+++ personal.sh
@@ -0,0 +1,63 @@
+apt update
+
+# install eza
+apt install -y gpg
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+apt update
+apt install -y eza
+
+# install zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+echo "eval \"\$(zoxide init bash)\"" >> ~/.bashrc
+
+# Add aliases to the end of the user's .bashrc file
+echo "" >> ~/.bashrc
+echo "# Custom aliases" >> ~/.bashrc
+echo "alias bashconfig=\"code ~/.bashrc\"" >> ~/.bashrc
+echo "alias reload=\"source ~/.bashrc\"" >> ~/.bashrc
+echo "alias la=\"eza -l --no-permissions --no-user -a --group-directories-first\"" >> ~/.bashrc
+echo "alias gs='git status'" >> ~/.bashrc
+echo "alias ga='git add'" >> ~/.bashrc
+echo "alias gp='git push'" >> ~/.bashrc
+echo "alias gc='git commit'" >> ~/.bashrc
+echo "alias cp='cp -iv'" >> ~/.bashrc
+echo "alias mv='mv -iv'" >> ~/.bashrc
+echo "alias cl='clear'" >> ~/.bashrc
+echo "alias ipinfo='curl -s http://ip-api.com/json/ | jq \"" >> ~/.bashrc
+
+# Bind keys for history search (eg. only show matches from the current line)
+autoload -Uz compinit && compinit
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
+
+eval "$(zoxide init --cmd cd zsh)"
+
+# reload the .bashrc file
+source ~/.bashrc
+
+# Make Bash completion case-insensitive
+bind "set completion-ignore-case on"
+
+# Disable menu completion if it has been enabled
+bind 'set show-all-if-ambiguous off'
+bind 'set menu-complete off'
+
+
+# Set the history file
+HISTFILE=~/.bash_history
+
+# Set the maximum number of history entries
+HISTSIZE=10000
+HISTFILESIZE=10000
+
+# Control history duplicates and append behavior
+HISTCONTROL=ignoreboth  # equivalent to hist_ignore_dups and hist_ignore_all_dups
+shopt -s histappend     # equivalent to appendhistory
+
+# Ensure history is shared between terminals
+PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
+source ~/.bashrc
