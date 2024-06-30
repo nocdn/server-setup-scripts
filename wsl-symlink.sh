a459165b231c7e5118bfb6eb116ec5bcaed7769b
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2024-06-30 20:30:30 +0100

    Fixed the input of the username

--- wsl-symlink.sh
+++ wsl-symlink.sh
@@ -1,4 +1,6 @@
-read "windows_username?enter the windows username: "
+#!/bin/bash
+
+read -p "enter the windows username: " windows_username
 
 ln -s /mnt/c/Users/$windows_username ./winhome
 ln -s /mnt/c/Users/$windows_username/curseforge/minecraft/Instances ./mcinstances
