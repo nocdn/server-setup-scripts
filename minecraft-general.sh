62cb8fe862436a615d863ebfe3811cab20467363
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2024-07-06 16:25:16 +0000

    added chmod +x to the adoptium script

--- minecraft-general.sh
+++ minecraft-general.sh
@@ -6,7 +6,15 @@ sudo apt upgrade -y
 
 # installing adoptium jre
 curl -O "https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/install-adoptium-jre.sh"
-sudo ./install-adoptium-jre.sh
+
+# Check if the script was downloaded
+if [ -f "./install-adoptium-jre.sh" ]; then
+    chmod +x ./install-adoptium-jre.sh
+    sudo ./install-adoptium-jre.sh
+else
+    echo "Failed to download install-adoptium-jre.sh"
+    exit 1
+fi
 
 # configure aws cli:
 aws configure
@@ -17,14 +25,14 @@ aws configure
 HOME_DIR=$(eval echo ~${SUDO_USER})
 USER_NAME=${SUDO_USER:-$USER}
 
-curl -o "backup.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/backup.sh
-curl -o "secondaryBackup.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/secondaryBackup.sh
-curl -o "restore.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/restore.sh
-curl -o "modpack-download.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/modpack-download.sh
-chmod +x "backup.sh"
-chmod +x "secondaryBackup.sh"
-chmod +x "restore.sh"
-chmod +x "modpack-download.sh"
+curl -o "${HOME_DIR}/backup.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/backup.sh
+curl -o "${HOME_DIR}/secondaryBackup.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/secondaryBackup.sh
+curl -o "${HOME_DIR}/restore.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/restore.sh
+curl -o "${HOME_DIR}/modpack-download.sh" https://raw.githubusercontent.com/Kayetic/server-setup-scripts/main/modpack-download.sh
+chmod +x "${HOME_DIR}/backup.sh"
+chmod +x "${HOME_DIR}/secondaryBackup.sh"
+chmod +x "${HOME_DIR}/restore.sh"
+chmod +x "${HOME_DIR}/modpack-download.sh"
 
 echo "Java version:"
 java --version
